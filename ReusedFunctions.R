96cdb72686b601f01183c137d693a1210996322c
paison <paisonchang@gis.tw>
2022-06-09 15:09:19 +0800

    [ReusedFunctions] edit normal_outlier_range()

--- ReusedFunctions.R
+++ ReusedFunctions.R
@@ -8,11 +8,9 @@
 normal_outlier_range <- function(df, sinfo){
   
   if(!is.na(sinfo$NORMAL_P)){p = sinfo$NORMAL_P}; k = qnorm(1-p/2)
-  
-  df = df %>%
-    dplyr::summarise(lower_bound = mean(.data[[sinfo$VALUE_COL]]) - k*sd(.data[[sinfo$VALUE_COL]]),
-                     upper_bound = mean(.data[[sinfo$VALUE_COL]]) + k*sd(.data[[sinfo$VALUE_COL]]))
-  return(df)
+  x = df[[sinfo$VALUE_COL]]; Mean = mean(x); Sd = st(x)
+  y = list(lower_bound = Mean - k*Sd, upper_bound = Mean + k*Sd)
+  return(y)
 }
 
 #=============================================================================
